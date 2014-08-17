2603b59bb54da847907c092030cb5242c237ddae
kecinzer <kecinzer@gmail.com>
2014-08-17 17:17:15 +0200

    Remove Camera2 and Gallery2 from build
    I don't need those apps anymore
    
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- target/product/generic_no_telephony.mk
+++ target/product/generic_no_telephony.mk
@@ -23,12 +23,10 @@ PRODUCT_PACKAGES := \
     DeskClock \
     Bluetooth \
     Calculator \
-    Camera2 \
     CertInstaller \
     Email \
     Exchange2 \
     FusedLocation \
-    Gallery2 \
     InputDevices \
     Keyguard \
     LatinIME \
